383cb024305b883df443fe7a03bfb7fe8f17bf65
Bui Kiem <buivantrung1105@gmail.com>
2019-09-28 12:56:28 +0700

    Add Formik hoc to SignIn component

--- src/shop/components/SignIn/SignIn.js
+++ src/shop/components/SignIn/SignIn.js
@@ -1,65 +1,90 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { withFormik } from "formik";
 import { Button, Modal, Form, Input, Icon, Checkbox } from "antd";
 
 import classes from "./SignIn.module.css";
 
-export const SignIn = ({ visible, handleClose }) => {
-  const [loading, setLoading] = useState(false);
+export const SignIn = withFormik({
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
+    await console.log(values);
+  }
+})(
+  ({
+    values,
+    visible,
+    handleClose,
+    handleChange,
+    handleBlur,
+    handleSubmit
+  }) => {
+    const [loading, setLoading] = useState(false);
 
-  const handleOk = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      handleClose();
-    }, 3000);
-  };
+    const handleOk = () => {
+      setLoading(true);
+      handleSubmit();
+      setTimeout(() => {
+        setLoading(false);
+        handleClose();
+      }, 3000);
+    };
 
-  return (
-    <Modal
-      visible={visible}
-      title="Sign In"
-      onOk={handleOk}
-      onCancel={handleClose}
-      footer={[
-        <Button key="back" onClick={handleClose}>
-          Return
-        </Button>,
-        <Button
-          key="submit"
-          type="primary"
-          loading={loading}
-          onClick={handleOk}
-        >
-          Sign In
-        </Button>
-      ]}
-    >
-      <div>
-        <Form.Item>
-          <Input
-            autoFocus
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Input
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Checkbox>Remember me</Checkbox>
-          <a className={classes.loginFormForgot} href="#">
-            Forgot password
-          </a>
-        </Form.Item>
-      </div>
-    </Modal>
-  );
-};
+    return (
+      <Modal
+        visible={visible}
+        title="Sign In"
+        onOk={handleOk}
+        onCancel={handleClose}
+        footer={[
+          <Button key="back" onClick={handleClose}>
+            Return
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={loading}
+            onClick={handleOk}
+          >
+            Sign In
+          </Button>
+        ]}
+      >
+        <div>
+          <Form.Item>
+            <Input
+              autoFocus
+              name="email"
+              type="email"
+              prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="E-mail"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name="password"
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox>Remember me</Checkbox>
+            <a className={classes.loginFormForgot} href="#">
+              Forgot password
+            </a>
+          </Form.Item>
+        </div>
+      </Modal>
+    );
+  }
+);
 
 SignIn.propTypes = {
   visible: PropTypes.bool.isRequired,
