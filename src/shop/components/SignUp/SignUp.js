52cec6580c64f8f816919de7720756d92973eeb7
Bui Kiem <buivantrung1105@gmail.com>
2019-09-28 15:46:19 +0700

    Create Sign Up Form

--- src/shop/components/SignUp/SignUp.js
+++ src/shop/components/SignUp/SignUp.js
@@ -1,15 +1,44 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Button, Modal } from "antd";
+import { Button, Icon, Modal } from "antd";
+import { Field, withFormik } from "formik";
+import * as yup from "yup";
 
-export const SignUp = ({ visible, handleClose }) => {
+import { emailValidation, passwordValidation } from "../../validationSchemas";
+import { CustomInputField } from "../CustomInputField/CustomInputField";
+
+const validationSchema = yup.object().shape({
+  email: emailValidation,
+  password: passwordValidation,
+  password2: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords do not match")
+    .required()
+});
+
+const initialValues = { email: "", password: "", password2: "" };
+
+export const SignUp = withFormik({
+  validationSchema,
+  mapPropsToValues: () => initialValues,
+  handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
+    await console.log(values);
+  }
+})(({ visible, handleClose, handleSubmit, resetForm, isValid }) => {
   const [loading, setLoading] = useState(false);
+
+  const handleCancel = () => {
+    handleClose();
+    resetForm(initialValues);
+  };
 
   const handleOk = () => {
     setLoading(true);
+    handleSubmit();
     setTimeout(() => {
       setLoading(false);
       handleClose();
+      resetForm();
     }, 3000);
   };
 
@@ -18,9 +47,9 @@ export const SignUp = ({ visible, handleClose }) => {
       visible={visible}
       title="Sign Up"
       onOk={handleOk}
-      onCancel={handleClose}
+      onCancel={handleCancel}
       footer={[
-        <Button key="back" onClick={handleClose}>
+        <Button key="back" onClick={handleCancel}>
           Return
         </Button>,
         <Button
@@ -28,19 +57,38 @@ export const SignUp = ({ visible, handleClose }) => {
           type="primary"
           loading={loading}
           onClick={handleOk}
+          disabled={!isValid}
         >
           Sign Up
         </Button>
       ]}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <div>
+        <Field
+          name="email"
+          type="email"
+          prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
+          placeholder="email"
+          component={CustomInputField}
+        />
+        <Field
+          name="password"
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="password"
+          placeholder="Password"
+          component={CustomInputField}
+        />
+        <Field
+          name="password2"
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="password"
+          placeholder="Confirm Password"
+          component={CustomInputField}
+        />
+      </div>
     </Modal>
   );
-};
+});
 
 SignUp.propTypes = {
   visible: PropTypes.bool.isRequired,
