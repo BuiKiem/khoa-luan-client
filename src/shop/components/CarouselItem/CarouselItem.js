888b96fbbf396753cc737e104663ad789821d3ba
Bui Kiem <buivantrung1105@gmail.com>
2019-09-28 22:01:28 +0700

    Add navigating arrows and styling

--- src/shop/components/CarouselItem/CarouselItem.js
+++ src/shop/components/CarouselItem/CarouselItem.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Button } from "antd";
 
 import classes from "./CarouselItem.module.css";
-import { Button } from "antd";
 
 export const CarouselItem = () => {
   return (
     <div className={classes.carouselItem}>
-      <Button>Detail</Button>
+      <Button className={classes.alignBottom} type="default" size="large">
+        Detail
+      </Button>
     </div>
   );
 };
--- src/shop/components/PromotionCarousel/PromotionCarousel.js
+++ src/shop/components/PromotionCarousel/PromotionCarousel.js
@@ -1,15 +1,35 @@
 import React from "react";
-import { Carousel } from "antd";
+import { Button, Carousel, Col, Icon, Row } from "antd";
 
 import { CarouselItem } from "../CarouselItem/CarouselItem";
 
+import classes from "./PromotionCarousel.module.css";
+
 export const PromotionCarousel = () => {
+  let carousel = React.createRef();
+  const next = () => carousel.next();
+  const previous = () => carousel.prev();
+
   return (
-    <Carousel autoplay>
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-    </Carousel>
+    <Row className={classes.carouselContainer}>
+      <Col span={1} className={classes.carouselController}>
+        <Button size="large" onClick={previous}>
+          <Icon type="left" />
+        </Button>
+      </Col>
+      <Col span={22}>
+        <Carousel ref={node => (carousel = node)} autoplay>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Col>
+      <Col span={1} className={classes.carouselController}>
+        <Button size="large" onClick={next}>
+          <Icon type="right" />
+        </Button>
+      </Col>
+    </Row>
   );
 };
