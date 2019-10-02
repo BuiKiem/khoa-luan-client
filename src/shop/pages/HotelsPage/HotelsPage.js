c4cc72daaa76b2aad37b4dbd5f1b3ad19c37f862
Bui Kiem <buivantrung1105@gmail.com>
2019-10-02 20:14:01 +0700

    Create hotels page layout

--- src/shop/pages/HotelsPage/HotelsPage.js
+++ src/shop/pages/HotelsPage/HotelsPage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Row, Col, Button } from "antd";
+
+import classes from "./HotelsPage.module.css";
 
 export const HotelsPage = () => {
-  return <h1>Hotels Page</h1>;
+  return (
+    <div>
+      <Row type="flex" justify="center" className={classes.hotelsPageRow}>
+        <Col span={20}>
+          <div style={{ backgroundColor: "white" }}>
+            <h1>SearchBox</h1>
+          </div>
+        </Col>
+      </Row>
+
+      <Row type="flex" justify="center" className={classes.hotelsPageRow}>
+        <Col span={6} className={classes.filterContainer}>
+          <div style={{ backgroundColor: "white" }}>
+            <h1>Filter</h1>
+          </div>
+        </Col>
+        <Col span={14} className={classes.hotelsListContainer}>
+          <div style={{ backgroundColor: "white" }}>
+            <h1>List</h1>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
