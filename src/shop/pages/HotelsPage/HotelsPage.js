0a231e28250e5319c82df6f6251014f7ea825f1d
Bui Kiem <buivantrung1105@gmail.com>
2019-10-04 11:38:34 +0700

    Dynamically display hotel search form

--- src/shop/pages/HotelsPage/HotelsPage.js
+++ src/shop/pages/HotelsPage/HotelsPage.js
@@ -1,16 +1,37 @@
-import React from "react";
-import { Row, Col, Button } from "antd";
+import React, { useState } from "react";
+import { Row, Col, Button, Typography } from "antd";
+
+import { SearchHotelForm } from "../../components/SearchHotelForm/SearchHotelForm";
 
 import classes from "./HotelsPage.module.css";
 
 export const HotelsPage = () => {
+  const [showSearchForm, setShowSearchForm] = useState(false);
   return (
     <div>
       <Row type="flex" justify="center" className={classes.hotelsPageRow}>
         <Col span={20}>
-          <div style={{ backgroundColor: "white" }}>
-            <h1>SearchBox</h1>
-          </div>
+          <Row>
+            <Col span={16}>
+              <Typography.Title level={3}>
+                Your search result in [Ho Chi Minh City]
+              </Typography.Title>
+              <Typography.Title level={4} type="secondary">
+                [Oct 3, 2019 - Oct 4, 2019]
+              </Typography.Title>
+            </Col>
+            <Col span={8}>
+              <Button
+                type="secondary"
+                block
+                size="large"
+                onClick={() => setShowSearchForm(!showSearchForm)}
+              >
+                CHANGE SEARCH
+              </Button>
+            </Col>
+          </Row>
+          {showSearchForm && <SearchHotelForm />}
         </Col>
       </Row>
 
