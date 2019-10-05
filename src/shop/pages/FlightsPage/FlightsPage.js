44fd9d5a426bb1d5cfae9f9fafeb66717024ecc0
Bui Kiem <buivantrung1105@gmail.com>
2019-10-05 13:56:32 +0700

    Reuse SearchDescription component

--- src/shop/pages/FlightsPage/FlightsPage.js
+++ src/shop/pages/FlightsPage/FlightsPage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { SearchDescription } from "../../components/SearchDescription/SearchDescription";
+import {SearchFlightForm} from "../../components/SearchFlightForm/SearchFlightForm";
+
+const searchDescriptionData = {
+  mainDescription:
+    "Hanoi (HAN)-Noibai International Airport ⇆ Ho Chi Minh City (SGN)-Tansonnhat Intl",
+  subDescriptions: [
+    "Saturday, 05 October 2019 - Monday, 07 October 2019",
+    "1 Adult | Economy"
+  ]
+};
 
 export const FlightsPage = () => {
-  return <h1>Flights Page</h1>;
+  const [showSearchForm, setShowSearchForm] = useState(false);
+
+  const toggleShowSearchForm = () => setShowSearchForm(!showSearchForm);
+  return (
+    <div>
+      <SearchDescription
+        show={showSearchForm}
+        toggleShow={toggleShowSearchForm}
+        searchForm={<SearchFlightForm />}
+        {...searchDescriptionData}
+      />
+    </div>
+  );
 };
