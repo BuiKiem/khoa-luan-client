4eeff751f32fc9b1180f8aff139400d0fc812af2
Bui Kiem <buivantrung1105@gmail.com>
2019-10-06 20:43:51 +0700

    Layout promotions list

--- src/shop/components/PromotionsList/PromotionsList.js
+++ src/shop/components/PromotionsList/PromotionsList.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { List, Card } from "antd";
+
+const data = [
+  {
+    title: "Title 1"
+  },
+  {
+    title: "Title 2"
+  },
+  {
+    title: "Title 3"
+  },
+  {
+    title: "Title 4"
+  }
+];
 
 export const PromotionsList = () => {
-  return <h1>Promotions List</h1>;
+  return (
+    <List
+      grid={{ gutter: 8, column: 2 }}
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <Card title={item.title}>Card Content</Card>
+        </List.Item>
+      )}
+    />
+  );
 };
