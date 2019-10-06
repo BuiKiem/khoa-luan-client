ff50f540f0c19c725bdc06d28f9d1315b571df65
Bui Kiem <buivantrung1105@gmail.com>
2019-10-06 12:56:28 +0700

    Toggle between one way and round trip flight

--- src/shop/pages/FlightsPage/FlightsPage.js
+++ src/shop/pages/FlightsPage/FlightsPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Col, Row } from "antd";
+import { Button, Col, Row, Tabs } from "antd";
 
 import { FlightList } from "../../components/FlightList/FlightList";
 import { SearchDescription } from "../../components/SearchDescription/SearchDescription";
 import { SearchFlightForm } from "../../components/SearchFlightForm/SearchFlightForm";
+
+const { TabPane } = Tabs;
 
 const searchDescriptionData = {
   mainDescription:
@@ -15,9 +17,26 @@ const searchDescriptionData = {
 };
 
 export const FlightsPage = () => {
+  const [isOneWay, setIsOneWay] = useState(true);
   const [showSearchForm, setShowSearchForm] = useState(false);
 
+  const toggleIsOneWay = () => setIsOneWay(!isOneWay);
   const toggleShowSearchForm = () => setShowSearchForm(!showSearchForm);
+
+  const flightList = isOneWay ? (
+    <FlightList />
+  ) : (
+    <Tabs defaultActiveKey="1">
+      <TabPane tab="Departure Flight" key="1">
+        <h1>Departure</h1>
+        <FlightList />
+      </TabPane>
+      <TabPane tab="Arrival Flight" key="2">
+        <h1>Arrival</h1>
+        <FlightList />
+      </TabPane>
+    </Tabs>
+  );
   return (
     <div>
       <SearchDescription
@@ -27,10 +46,10 @@ export const FlightsPage = () => {
         {...searchDescriptionData}
       />
 
+      <Button onClick={toggleIsOneWay}>TOGGLE</Button>
+
       <Row type="flex" justify="center">
-        <Col span={20}>
-          <FlightList />
-        </Col>
+        <Col span={20}>{flightList}</Col>
       </Row>
     </div>
   );
